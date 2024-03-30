b6dd81aca7049dffaad49a5a484f480e5cd3593d
joseftango <4279@holbertonschool.com>
2024-03-30 03:43:19 +0100

    5-json_to_user.dart

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,24 @@
+
+class User {
+
+  late int id;
+  late String name;
+  late int age;
+  late double height;
+
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(id: userJson['id'], name: userJson['name'],
+    age: userJson['age'], height: userJson['height']);
+  }
+
+  Map toJson() {
+    return {'id': id, 'name': name, 'age': age, 'height': height};
+  }
+
+  String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height)";
+  }
+
+}
