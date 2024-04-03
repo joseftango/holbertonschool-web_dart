fd7f067328bc81c8a11b75133e799835c65fbf12
joseftango <4279@holbertonschool.com>
2024-04-03 20:04:41 +0100

    0-users_count.dart

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -2,12 +2,7 @@
 import 'dart:async';
 
 Future<void> usersCount() async {
-  Future<int> fetchUsersCount() => Future.delayed( Duration(seconds: 2), () => 19 );
+  Future<int> fetchUsersCount() => Future.delayed( Duration(seconds: 5), () => 19 );
   int count = await fetchUsersCount();
   print(count);
 }
-
-main() async {
-  await usersCount();
-}
-
