63e9318ede36a61c41e6eff47a6090a734268d4e
joseftango <4279@holbertonschool.com>
2024-04-03 21:18:00 +0100

    0-users_count.dart

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,6 +1,8 @@
+Future<int> fetchUsersCount() => Future.delayed(
+      const Duration(seconds: 2),
+      () => 19,
+    );
 
-Future<int> fetchUsersCount() => Future.delayed( Duration(seconds: 2), () => 19 );
 usersCount() async {
-  int count = await fetchUsersCount();
-  print(count);
+  print(await fetchUsersCount());
 }
